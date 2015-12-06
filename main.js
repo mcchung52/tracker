e79c53a454bd58fb9f0ede989323a0651a97296f
mcchung52 <mcchung52@hotmail.com>
2015-12-06 13:11:42 -0800

    separated out to main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+var map, currLoc, marker, wpid;
+var initial;
+var mapElement;
+var image;
+var geo_options = {
+  enableHighAccuracy: true, 
+  maximumAge        : 0 
+//   timeout           : 27000
+};
+
+var ref = new Firebase("https://fbex52.firebaseio.com/");
+
+function initMap() {
+  initial = true;
+  mapElement = document.getElementById("map");
+  image = {
+    url: 'http://uxrepo.com/static/icon-sets/windows/png32/256/000000/location-circle-256-000000.png',
+    scaledSize: new google.maps.Size(20, 20),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(10, 10)
+  };
+
+  if (!navigator.geolocation){
+    mapElement.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }      
+  
+  wpid = navigator.geolocation.watchPosition(geo_success, function(){}, geo_options);
+}
+
+function geo_success(pos) {
+  console.log('position:', pos);
+  currLoc = {
+    lat: pos.coords.latitude,
+    lng: pos.coords.longitude
+  };
+  
+  if (initial) {
+    map = new google.maps.Map(mapElement, {
+      center: currLoc,
+      zoom: 15
+    });
+    marker = new google.maps.Marker({
+      position: currLoc,
+      title: 'Me!',
+      map: map,
+      icon: image
+    });
+    initial = false;            
+  } else {
+    //marker.setMap(null);
+    map.setCenter(currLoc);
+    // marker.setMap(map);
+    marker.setPosition(currLoc);
+  }
+  // $.ajax({
+  //   url: 'http://localhost:3000/report',
+  //   method: 'post',
+  //   data: currLoc
+  // })
+  // .done(function(data){
+  //   console.log(data);
+  // });
+  ref.push(currLoc);
+}
